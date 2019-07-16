bf6ac5483ca13f0236d8fe1dfacb3d7f6c3f8984
YuunaHasegawa <y.hasegawa0313@gmail.com>
2019-07-16 21:02:08 +0900

    配列の勉強の追加

--- Chap3.js
+++ Chap3.js
@@ -33,21 +33,38 @@ for (let cnt = 0; cnt <= 10; cnt++) {
 // chap3-4-1
 // 逆順で繰り返す（1ずつ減らす）
 
-for (let cnt = 10; cnt > 0; cnt--){
-    console.log(cnt + "回目のメッセージです");
+for (let cnt = 10; cnt > 0; cnt--) {
+  console.log(cnt + "回目のメッセージです");
 }
 
 // chap3-5-1
 // 99算の表示
 
-for (let x = 1; x < 10; x++){
-    for(let y = 1; y < 10; y++){
-        console.log(x * y);
-    }
+for (let x = 1; x < 10; x++) {
+  for (let y = 1; y < 10; y++) {
+    console.log(x * y);
+  }
 }
 
-for (let x = 1; x < 10; x++){
-    for(let y = 1; y < 10; y++){
-        console.log(x + "×" + y + "=" + x * y);
-    }
+for (let x = 1; x < 10; x++) {
+  for (let y = 1; y < 10; y++) {
+    console.log(x + "×" + y + "=" + x * y);
+  }
 }
+
+// chap3-6-1
+// 配列を作って利用する
+
+// 「火」と表示
+let wdays = ['月','火','水','木','金','土','日'];
+    console.log(wdays[1]);
+
+// 「土」と表示
+let wdays = ['月','火','水','木','金','土','日'];
+console.log(wdays[5]);
+
+// chap3-6-2
+// 配列の要素を書き換える
+let wdays = ['月','火','水','木','金','土','日'];
+wdays[1] = '炎'
+console.log(wdays);
