87f25a7acc060eb65c4ed92fdd37d5d2635ab7af
YuunaHasegawa <y.hasegawa0313@gmail.com>
2019-07-16 21:03:08 +0900

    配列の勉強の追加

---
+++ array_graph.js
@@ -0,0 +1,20 @@
+// NG
+let graph = ["*","**","***","****","*****"];
+console.log(graph);
+
+// 数を表示させる
+let array = [1,2,3];
+for(let num of array){
+    console.log(num);
+}
+
+// ＊を表示させてグラフにする
+let sign = "★";
+let array = [1,2,3,5,8,4,2];
+for(let num of array){
+    let out  = "";
+    for(let i = 0; i<num; i++){
+        out = out + sign;
+    } 
+    console.log(out);
+}
