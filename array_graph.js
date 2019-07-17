8265cc4ba6233f6e619fcfdb4b642bee4abb4a1a
yuuna <y.hasegawa0313@gmail.com>
2019-07-17 12:38:55 +0900

    出力結果の追記

--- array_graph.js
+++ array_graph.js
@@ -1,7 +1,3 @@
-// NG
-let graph = ["*","**","***","****","*****"];
-console.log(graph);
-
 // 数を表示させる
 let array = [1,2,3];
 for(let num of array){
@@ -9,14 +5,25 @@ for(let num of array){
 }
 
 // ＊を表示させてグラフにする
-let sign = "★";
+let sign = "*";
 let array = [1,2,3,5,8,4,2];
 for(let num of array){
     let out  = "";
     for(let i = 0; i<num; i++){
         out = out + sign;
-    } 
+    }
     console.log(out);
 }
 
-    
+/* 出力結果
+
+*
+**
+***
+*****
+********
+****
+**
+
+*/
+
